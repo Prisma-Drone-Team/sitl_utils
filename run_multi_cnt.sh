b037b7de49a0ec6e08ceba1a7ac2e42cf36db94a
Simone-DAngelo <simone.dang96@gmail.com>
2026-02-27 18:40:48 +0100

    multi_robot simulation

---
+++ run_multi_cnt.sh
@@ -0,0 +1,21 @@
+
+# docker exec -itu 0 px4-ros2 bash  // run root
+
+# enable access to xhost from the container
+xhost +
+
+
+# Run docker and open bash shell 
+docker run --rm -it --privileged \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-v "/dev:/dev" \
+-v $(pwd)/PX4-Autopilot:/root/PX4-Autopilot:rw \
+-v $(pwd)/PX4_neabotics:/root/PX4_neabotics:rw \
+-v $(pwd)/ros2_ws-src/px4_ros_com:/root/ros2_ws/src/px4_ros_com:rw \
+-v $(pwd)/ros2_ws-src/pkg:/root/ros2_ws/src/pkg:rw \
+-v ~/docker_dev/rover_sim_motion_stack/src/pkg:/home/user/rover_ws/src/pkg \
+--env="DISPLAY=$DISPLAY" \
+--network host \
+--name=leo-cnt leo-img bash
+
+
